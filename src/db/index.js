05f59f7e2571cc6c3a42f47225d8859b7fe1ed99
PritamIsHere <pritamkar98765@gmail.com>
2024-02-05 21:26:21 +0530

    add user and video model

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connecionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connecionInstance = await mongoose.connect(`mongodb+srv://pritamkar98765:pritam123@pritam.qq8wggv.mongodb.net/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST": ${connecionInstance.connection.host}`);
     } catch (err) {
         console.log("MONGODB connection FAILED ", err);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import dotenv from "dotenv";
 // import { DB_NAME } from "./constants"
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path : './env' 
-})
 
 
 connectDB()
